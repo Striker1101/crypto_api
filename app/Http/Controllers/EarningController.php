80e446ab078af850e7121f404f826750329e3c78
striker <goodluckodemilin@gmail.com>
2024-02-10 12:40:59 +0100

    added resources for earing and investment

---
+++ app/Http/Controllers/EarningController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Earning;
+use App\Http\Requests\StoreEarningRequest;
+use App\Http\Requests\UpdateEarningRequest;
+
+class EarningController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEarningRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Earning $earning)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Earning $earning)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEarningRequest $request, Earning $earning)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Earning $earning)
+    {
+        //
+    }
+}
